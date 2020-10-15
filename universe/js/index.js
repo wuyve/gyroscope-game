e251f96f2eb03124abf3face062d15018ea48426
吴月 <wuyue1@joyoung.com>
2020-10-15 17:40:35 +0800

    '上传地球转动'

---
+++ universe/js/index.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    let sun = new Image();
+    let earth = new Image();
+    let moon = new Image();
+
+    // 初始化
+    function init() {
+        sun.src = './img/sun.png';
+        sun.width = 50;
+        sun.height = 50;
+        earth.src = './img/earth.png';
+        moon.src = './img/moon.png';
+        window.requestAnimationFrame(draw);
+    }
+    let sunWidth = 150, sunHeight = 150;
+    let vx = 0.5, vy = 0.5;
+    let x = 115, y = 115;
+
+    function draw() {
+        // 定义宇宙画布
+        let universe = document.getElementById('universe');
+        let uni_ctx = universe.getContext('2d');
+
+        uni_ctx.globalCompositeOperation = 'destination-over';
+        uni_ctx.clearRect(0, 0, 300, 300);
+
+        uni_ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+
+        uni_ctx.save();
+        uni_ctx.translate(150, 150);
+
+        // Earth
+        var time = new Date();
+        uni_ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+        uni_ctx.translate(105, 0);
+        uni_ctx.drawImage(earth, -27, -27, 50, 50);
+
+        // Moon
+        uni_ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
+        uni_ctx.translate(0, 28.5);
+        uni_ctx.drawImage(moon, -10, -10, 25, 25);
+
+        uni_ctx.restore();
+
+        uni_ctx.beginPath();
+        uni_ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // 地球运动轨迹
+        uni_ctx.stroke();
+
+        // Sun
+        uni_ctx.drawImage(sun, x, y, sunWidth, sunHeight);
+        sunWidth += vx, sunHeight += vy;
+        x = 150 - sunWidth / 2, y = 150 - sunHeight / 2;
+        if (sunWidth > 200 || sunWidth < 150) {
+            vx = -vx, vy = -vy
+        }
+        window.requestAnimationFrame(draw);
+    }
+    init();
+
+    function balanceCar() {
+        let car = document.getElementById('car');
+        let ctx_car = car.getContext('2d');
+
+        let carImg = new Image();
+        carImg.src = './img/car.png';
+        ctx_car.globalCompositeOperation = 'destination-over';
+        ctx_car.drawImage(carImg, 50, 50, 50, 100);
+
+    }
+    balanceCar();
+});
